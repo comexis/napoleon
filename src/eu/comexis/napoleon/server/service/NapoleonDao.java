228ebbdd2c745ca5465762bfba70a72990ba3350
xplatiau <xavier.platiaux@comexis.eu>
2012-01-20 17:19:34 +0100

    add some implementation for Dao + tests

--- src/eu/comexis/napoleon/server/service/NapoleonDao.java
+++ src/eu/comexis/napoleon/server/service/NapoleonDao.java
@@ -3,6 +3,9 @@
  */
 package eu.comexis.napoleon.server.service;
 
+import java.util.Map;
+
+import com.google.appengine.api.datastore.Key;
 import com.googlecode.objectify.ObjectifyService;
 import com.googlecode.objectify.util.DAOBase;
 
@@ -26,5 +29,67 @@ public class NapoleonDao extends DAOBase{
     }
 	public NapoleonDao() {
 		// TODO Auto-generated constructor stub
+	}
+	public Owner createOwner(){
+		Owner owner = new Owner();
+		return owner;
+	}
+	public Boolean updateOwner(Owner owner){
+		try{
+			ofy().put(owner);
+			return true;
+		}catch (Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Boolean deleteOwner(Owner owner){
+		try{
+			ofy().delete(owner);
+			return true;
+		}catch (Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Owner findOwnerByName(String name){
+		try{
+			Owner owner = ofy().query(Owner.class).filter("lastName", name).get();
+			return owner;
+		}catch (Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public Tenant createTenant(){
+		Tenant tenant = new Tenant();
+		return tenant;
+	}
+	public Boolean updateTenant(Tenant tenant){
+		try{
+			ofy().put(tenant);
+			return true;
+		}catch (Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Boolean deleteTenant(Tenant tenant){
+		try{
+			ofy().delete(tenant);
+			return true;
+		}catch (Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Tenant findTenantByName(String name){
+		try{
+			Tenant tenant = ofy().query(Tenant.class).filter("lastName", name).get();
+			return tenant;
+		}catch (Exception e){
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
--- src/eu/comexis/napoleon/server/service/NapoleonServiceImpl.java
+++ src/eu/comexis/napoleon/server/service/NapoleonServiceImpl.java
@@ -16,16 +16,16 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 public class NapoleonServiceImpl extends RemoteServiceServlet implements NapoleonService{
 	public static NapoleonDao model = new NapoleonDao();
 	// CREATE
-	public Tenant createTenant(){return null;}
+	public Tenant createTenant(){return model.createTenant();}
 	public RealEstate createRealEstate(){return null;}
-	public Owner createOwner(){return null;}
+	public Owner createOwner(){return model.createOwner();}
 	public Condo createCondo(){return null;}
 	public Lease createLease(Long realEstateId,Long tenantId,Date startDate, Date endDate){return null;}
 	// READ
 	public Tenant findTenantById(Long id){return null;}
-	public Tenant findTenantByName(String name){return null;}
+	public Tenant findTenantByName(String name){return model.findTenantByName(name);}
 	public Owner findOwnerById(Long id){return null;}
-	public Owner findOwnerByName(String name){return null;}
+	public Owner findOwnerByName(String name){return model.findOwnerByName(name);}
 	public RealEstate findRealEstateById(Long id){return null;}
 	public RealEstate findRealEstateByRef(String reference){return null;}
 	public Condo findCondoById(Long id){return null;}
@@ -36,14 +36,14 @@ public class NapoleonServiceImpl extends RemoteServiceServlet implements Napoleo
 	public Lease findActualLeaseForRealEstate(Long realEstateId){return null;}
 	public Lease[] findLeaseHistoryForRealEstate(Long realEstateId){return null;}
 	// UPDATE
-	public Boolean updateOwner(Owner owner){return null;}
-	public Boolean updateTenant(Tenant tenant){return null;}
+	public Boolean updateOwner(Owner owner){return model.updateOwner(owner);}
+	public Boolean updateTenant(Tenant tenant){return model.updateTenant(tenant);}
 	public Boolean updateRealEstate(RealEstate rental){return null;}
 	public Boolean updateCondo(Condo condo){return null;}
 	public Boolean updateLease(Lease lease){return null;}
 	// DELETE
-	public Boolean deleteOwner(Owner owner){return null;}
-	public Boolean deleteTenant(Tenant tenant){return null;}
+	public Boolean deleteOwner(Owner owner){return model.deleteOwner(owner);}
+	public Boolean deleteTenant(Tenant tenant){return model.deleteTenant(tenant);}
 	public Boolean deleteRealEstate(RealEstate rental){return null;}
 	public Boolean deleteCondo(Condo condo){return null;}
 	public Boolean deleteLease(Lease lease){return null;}
