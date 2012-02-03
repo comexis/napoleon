3e65c85b8d02a8f270f01dab5a4342b85d1293c1
comexis <comexis.partners@gmail.com>
2012-02-03 18:38:19 +0100

    use Title.valueOf(string) instead of custom method

--- src/eu/comexis/napoleon/shared/model/Title.java
+++ src/eu/comexis/napoleon/shared/model/Title.java
@@ -2,12 +2,4 @@ package eu.comexis.napoleon.shared.model;
 
 public enum Title {
   MR, MRS, MISS;
-  public static Title fromStringToEnum(String value){
-    for(Title t:values()){
-      if (t.name().equals(value)){
-        return t;
-      }
-    }
-    return null;
-  }
 }
