fcc163f360226e0523bcc3fe7bd03e8deafdd630
xavier <xavier@xavier-Dell-DXP051.(none)>
2012-03-15 11:19:17 +0100

    bug fixing: 65,61,51

--- src/eu/comexis/napoleon/shared/model/Title.java
+++ src/eu/comexis/napoleon/shared/model/Title.java
@@ -1,5 +1,5 @@
 package eu.comexis.napoleon.shared.model;
 
 public enum Title {
-  MR, MRS, MISS;
+  MR, MRS, MISS,MRS_MR,MR_MRS,PHYS,MORALEPRIV,MORALEPUB,GESTION,PUB,PRIV,IE,COM,SCI,SCRI,SC,SPRLU, SPRL,SCRL,SA,ASBL;
 }
